dd2a13ecc799b6bfd740578f2131294c7daa64f0
anatawa12 <anatawa12@icloud.com>
2021-11-10 17:12:10 +0900

    fix build tools for linux

--- install_deps.sh
+++ install_deps.sh
@@ -4,19 +4,32 @@ die() {
   exit 1
 }
 
-run_apt() {
+run_root() {
   if [ "$(id -u)" == 0 ]; then
-    apt "$@"
+    "$@"
   elif sudo --help 2>&1 >/dev/null; then
-    sudo apt "$@"
+    sudo "$@"
   else
     die "you are not root and no sudo command found."
   fi
 }
 
+run_apt() {
+  run_root apt "$@"
+}
+
 case "$1" in
   *-apple-darwin )       brew update && brew install automake git libtool shtool wget nasm ;;
-  *-unknown-linux-gnu )  run_apt update -qq && run_apt install -y autoconf automake build-essential git-core wget yasm;;
-  *-unknown-linux-musl ) run_apt update -qq && run_apt install -y autoconf automake build-essential git-core musl-tools wget yasm;;
   *-pc-windows-msvc )    choco install -y msys2 yasm;;
+  *-unknown-linux-* )
+    packages="autoconf automake build-essential git-core wget yasm"
+    case "$1" in
+      x86_64-* ) packages="$packages build-essential-amd64" ;;
+      aarch64-* ) packages="$packages build-essential-arm64" ;;
+    esac
+    case "$1" in
+      x86_64-*-musl ) packages="$packages musl-tools" ;;
+      aarch64-*-musl ) packages="$packages musl-tools:arm64" ;;
+    esac
+    run_apt update -qq && run_apt install -y $packages;;
 esac
