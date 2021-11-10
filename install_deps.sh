591563fe5d5c8c6e86f7f6a89e7df2b1e738c09c
anatawa12 <anatawa12@icloud.com>
2021-11-10 14:22:00 +0900

    fix /install_deps.sh

--- install_deps.sh
+++ install_deps.sh
@@ -6,17 +6,17 @@ die() {
 
 run_apt() {
   if [ "$(id -u)" == 0 ]; then
-    apt "$($@)"
+    apt "$@"
   elif sudo --help 2>&1 >/dev/null; then
-    sudo -- apt "$($@)"
+    sudo apt "$@"
   else
     die "you are not root and no sudo command found."
   fi
 }
 
 case "$1" in
-  *-apple-darwin )       brew update && brew install autoconf automake build-essential git-core wget yasm ;;
-  *-unknown-linux-gnu )  run_apt update -qq && run_apt -y install autoconf automake build-essential git-core wget yasm;;
-  *-unknown-linux-musl ) run_apt update -qq && run_apt -y install autoconf automake build-essential git-core musl-tools wget yasm;;
+  *-apple-darwin )       brew update && brew install automake git libtool shtool wget nasm ;;
+  *-unknown-linux-gnu )  run_apt update -qq && run_apt install -y autoconf automake build-essential git-core wget yasm;;
+  *-unknown-linux-musl ) run_apt update -qq && run_apt install -y autoconf automake build-essential git-core musl-tools wget yasm;;
   *-pc-windows-msvc )    choco install msys2 yasm;;
 esac
